aab952f6139d1cdc7418a3431de60bdb408693c1
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2026-02-26 20:15:27 +0100

    Creada logica de tirada aleatoria para 6 caras

---
+++ main.py
@@ -0,0 +1,7 @@
+# Queremos simular una tirada de un dado de 6 caras
+
+import random
+
+dado = [1, 2, 3, 4, 5, 6]  # caras
+tirada = random.choice(dado)  # escojo una cara de mis caras
+print(tirada)
